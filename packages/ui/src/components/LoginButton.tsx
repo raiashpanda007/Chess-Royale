d87b97089779b1d8d6615724e72b45f365546477
Ashwin <ashwin.2201098cs@iiitbh.ac.in>
2025-01-02 10:37:53 +0530

    Setting up the base project. 1.Added poppins font to ui folder and start setting up next-auth

--- packages/ui/src/components/LoginButton.tsx
+++ packages/ui/src/components/LoginButton.tsx
@@ -1,7 +1,9 @@
 import React from "react";
 import { signIn } from "next-auth/react";
+import { useSession } from "next-auth/react";
 import { Button } from "@workspace/ui/components/button";
 function LoginButton() {
+    const session = useSession();
   return <Button className="font-poppins font-semibold" onClick={() => signIn()}>Login</Button>;
 }
 
