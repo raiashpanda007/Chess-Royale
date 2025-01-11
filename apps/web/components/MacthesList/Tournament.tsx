2909cbf74dd141dbbaf2d06a1265d024dd8ab023
Ashwin <ashwin.2201098cs@iiitbh.ac.in>
2025-01-11 14:11:33 +0530

    added removed card

--- apps/web/components/MacthesList/Tournament.tsx
+++ apps/web/components/MacthesList/Tournament.tsx
@@ -1,5 +1,12 @@
 import { Button } from "@workspace/ui/components/button"
-
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+} from "@workspace/ui/components/card"
 import { Input } from "@workspace/ui/components/input"
 import { Label } from "@workspace/ui/components/label"
 import {
